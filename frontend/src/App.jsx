ba6056b436fe20cd8c1b9f6d1e883ad3a8e3cd59
VibhorSharma0053 <144813390+VibhorSharma0053@users.noreply.github.com>
2024-11-14 18:23:40 +0530

    Push 2 from VS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
   const [file, setFile] = useState(null);
   const [emailData, setEmailData] = useState({
-    recipientEmail: '',
-    subject: '',
-    message: ''
+    recipientEmail: "",
+    subject: "",
+    message: "",
   });
 
   const handleFileChange = (e) => {
@@ -18,67 +18,90 @@ function App() {
 
   const handleFileUpload = async () => {
     const formData = new FormData();
-    formData.append('file', file);
+    formData.append("file", file);
 
     try {
-      const response = await fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData
+      const response = await fetch("http://localhost:5000/upload", {
+        method: "POST",
+        body: formData,
       });
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.error('Error uploading file:', error);
+      console.error("Error uploading file:", error);
     }
   };
 
   const handleEmailSend = async () => {
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
-        method: 'POST',
+      const response = await fetch("http://localhost:5000/send-email", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(emailData)
+        body: JSON.stringify(emailData),
       });
       const data = await response.json();
       console.log(data);
     } catch (error) {
-      console.error('Error sending email:', error);
+      console.error("Error sending email:", error);
     }
   };
 
   return (
-    <div className=' w-full h-full'>
-      <h1 className='text-5xl font-bold bg-emerald-300 text-green-900 py-5 flex justify-center items-center'>Email Sender</h1>
-      <div className='p-3 flex gap-5 justify-center items-center w-full bg-amber-200'>
-        <label className='font-semibold'>Upload CSV:</label>
-        <input className='rounded font-semibold' type="file" onChange={handleFileChange} />
-        <button className='font-semibold text-xl bg-emerald-400 hover:ease-in hover:bg-emerald-200 rounded px-5 py-3 text-emerald-900 text-semibold' onClick={handleFileUpload}>Upload File</button>
+    <div className=" w-screen h-screen bg-lime-300">
+      <h1 className="text-5xl font-bold bg-emerald-300 text-green-900 py-5 flex justify-center items-center">
+        Email Sender
+      </h1>
+      <div className="p-3 flex gap-5 justify-center items-center w-full text-lg bg-lime-300">
+        <label className="font-semibold">Upload CSV:</label>
+        <input
+          className="rounded font-semibold"
+          type="file"
+          onChange={handleFileChange}
+        />
+        <button
+          className="font-semibold border-dashed border-emerald-900 border-4 text-xl bg-emerald-400 hover:ease-in hover:bg-emerald-200 rounded-lg px-5 py-3 text-emerald-900 text-semibold"
+          onClick={handleFileUpload}
+        >
+          Upload File
+        </button>
       </div>
-      <div className='w-full bg-lime-200'>
-        <h2>Send Email</h2>
-        <input
-          type="email"
-          name="recipientEmail"
-          placeholder="Recipient Email"
-          value={emailData.recipientEmail}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="subject"
-          placeholder="Subject"
-          value={emailData.subject}
-          onChange={handleInputChange}
-        />
-        <textarea
-          name="message"
-          placeholder="Message"
-          value={emailData.message}
-          onChange={handleInputChange}
-        ></textarea>
-        <button onClick={handleEmailSend}>Send Email</button>
+      <div className="w-full bg-lime-300">
+        <h2 className="text-green-900 place-content-center p-3 text-3xl font-bold">
+          Send Email
+        </h2>
+        <div className="flex p-2 px-9 gap-4 align-center flex-col justify-center">
+          <input
+            className="w-1/3 border-double border-4 border-sky-500 rounded-lg bg-emerald-100 px-4 py-2 text-xl"
+            type="email"
+            name="recipientEmail"
+            placeholder="Recipient Email"
+            value={emailData.recipientEmail}
+            onChange={handleInputChange}
+          />
+          <input
+            className="w-1/3 border-double border-4 border-sky-500 rounded-lg bg-emerald-100 px-4 py-2 text-xl"
+            type="text"
+            name="subject"
+            placeholder="Subject"
+            value={emailData.subject}
+            onChange={handleInputChange}
+          />
+          <textarea
+            className="w-1/3 border-double border-4 border-sky-500 rounded-lg bg-emerald-100 px-4 py-2 text-xl"
+            name="message"
+            placeholder="Message"
+            value={emailData.message}
+            onChange={handleInputChange}
+          ></textarea>
+          <button
+            className="bg-emerald-500 p-3 font-semibold rounded-xl hover:bg-emerald-200 text-lg w-1/5"
+            onClick={handleEmailSend}
+          >
+            Send Email
+          </button>
+        </div>
       </div>
     </div>
   );
